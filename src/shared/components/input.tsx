db724715a4423a5bf167dcd09ba95801fdaab565
Damilola Bada <damilolabada@Damilolas-MacBook-Air.local>
2024-10-20 22:39:12 +0200

    modified text color for input component

--- src/shared/components/input.tsx
+++ src/shared/components/input.tsx
@@ -3,7 +3,7 @@ import { cva, VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const inputVariants = cva(
-  'flex h-9 w-full rounded-md border border-solid bg-transparent px-3 py-1 text-sm text-input shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+  'flex h-9 w-full rounded-md border border-solid bg-transparent px-3 py-1 text-primary text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 dark:text-input',
   {
     variants: {
       variant: {
